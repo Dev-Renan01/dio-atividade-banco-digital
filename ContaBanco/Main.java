745d650aed75517d67c7cdd9f0e510322d15aa6b
Thiago Renan <thiagorenan1108@gmail.com>
2025-05-23 13:25:12 -0300

    Projeto adicionado com alterações!

--- ContaBanco/Main.java
+++ ContaBanco/Main.java
@@ -1,7 +1,19 @@
-
+package ContaBanco;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Projeto criado!");
+            Cliente Thiago = new Cliente();
+        Thiago.setNome("Thiago");
+
+            Conta cc = new ContaCorrente(Thiago);
+            Conta poupanca = new ContaPoupanca(Thiago);
+
+            cc.depositar(100);
+            cc.transferir(50, poupanca);
+
+            cc.imprimirExtrato();
+            poupanca.imprimirExtrato();
+
+            cc.consultarSaldo();
     }
 }
